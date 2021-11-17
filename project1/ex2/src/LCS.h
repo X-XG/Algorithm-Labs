cd1989c7bfab1c9337ec5b7e00733dad186f11f6
ROG1 <934162496@qq.com>
2021-11-17 06:20:13 -0800

    lcs

--- project1/ex2/src/LCS.h
+++ project1/ex2/src/LCS.h
@@ -1,44 +1,66 @@
 #include<cstdlib>
-#include<time.h>
+#include<vector>
+#include<string>
+
+#define MAX_NUM 32
 
 class LCS {
 private:
-    int num;
-    char *x;
-    char *y;
-    void GetLCS();
+    int len;
+    char* x;
+    char* y;
+    int c[MAX_NUM][MAX_NUM] = { 0 };
 
 public:
-    LCS(int num, char *x, char *y);
+    LCS(int len, char* x, char* y);
     ~LCS();
-    
-    int result;
-    int time;
-    void GetResult();
+
+    int result = 0;
+    std::vector<std::string> lcss;
+    void GetLCS();
+    void PrintLCS(int i, int j, std::string str_lcs);
 };
 
-LCS::LCS(int num, char *x, char *y){
-    this->num = num;
-    this->x = new char[num];
-    this->y = new char[num];
-    for(int i=0; i<num; i++){
+LCS::LCS(int len, char* x, char* y) {
+    this->len = len;
+    this->x = new char[len];
+    this->y = new char[len];
+    for (int i = 0; i < len; i++) {
         this->x[i] = x[i];
         this->y[i] = y[i];
     }
 }
 
-LCS::~LCS(){
-    delete [] x;
-    delete [] y;
+LCS::~LCS() {
+    delete[] x;
+    delete[] y;
 }
 
-void LCS::GetLCS(){
-
+void LCS::GetLCS() {
+    for (int i = 1;i <= len;i++) {
+        for (int j = 1; j <= len; j++) {
+            if (x[i-1] == y[j-1])c[i][j] = c[i - 1][j - 1] + 1;
+            else c[i][j] = c[i - 1][j] >= c[i][j - 1] ? c[i - 1][j] : c[i][j - 1];
+        }
+    }
+    printf("%d",c[len][len]);
 }
 
-void LCS::GetResult(){
-    int start = clock();
-    GetLCS();
-    int end = clock();
-    time = start - end;
+void LCS::PrintLCS(int i, int j, std::string str_lcs){
+    while (i>0 && j>0){
+        if(x[i-1]==y[j-1]){
+            str_lcs = x[i-1]+str_lcs;
+            i--;
+            j--;
+        }
+        else if(c[i-1][j]<c[i][j-1]) j--;
+        else if(c[i-1][j]>c[i][j-1]) i--;
+        else {
+            PrintLCS(i-1, j , str_lcs);
+            PrintLCS(i, j-1 ,str_lcs);
+            return;
+        }
+    }
+    result++;
+    lcss.push_back(str_lcs);
 }
--- project1/ex2/src/main.cpp
+++ project1/ex2/src/main.cpp
@@ -1,17 +1,19 @@
 #include<iostream>
 #include<fstream>
 #include<string>
+#include<chrono>
 
 #include "LCS.h"
 
 #define NUM_GROUP 5
 
 using namespace std;
+using namespace  chrono;
 
-int main(){
+int main() {
     string input_path = "../input/1_2_input.txt";
     string result_path_base = "../output/result_";
-    string time_path = "..output/time.txt";
+    string time_path = "../output/time.txt";
 
     ifstream infile;
     ofstream fresult;
@@ -19,29 +21,39 @@ int main(){
     infile.open(input_path);
     ftime.open(time_path);
 
-    for(int i=0; i<NUM_GROUP; i++){
-        int num;
-        infile >> num;
-        char *x = new char[num];
-        char *y = new char[num];
-        for(int j=0; j<num; j++){
+    for (int i = 0; i < NUM_GROUP; i++) {
+        int len;
+        infile >> len;
+        char* x = new char[len];
+        char* y = new char[len];
+        for (int j = 0; j < len; j++) {
             infile >> x[j];
         }
-        for(int j=0; j<num; j++){
+        for (int j = 0; j < len; j++) {
             infile >> y[j];
-        }  
+        }
 
-        LCS lcser(num, x, y);
+        LCS lcser(len, x, y);
         delete x;
         delete y;
 
-        string result_path = result_path_base + to_string(num) + ".txt";
+        string result_path = result_path_base + to_string(len) + ".txt";
         fresult.open(result_path);
 
+        auto start = system_clock::now();
+        lcser.GetLCS();
+        lcser.PrintLCS(len, len, "");
+        auto end = system_clock::now();
+        auto duration = duration_cast<nanoseconds>(end - start);
+        int time = int(duration.count()) * nanoseconds::period::num;
+
         fresult << lcser.result << endl;
+        for (auto str_lcs : lcser.lcss) {
+            fresult << str_lcs << endl;
+        }
         fresult.close();
-        
-        ftime << lcser.time << endl;
+
+        ftime << time << endl;
     }
 
     infile.close();
