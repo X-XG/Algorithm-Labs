83e2c0241fba8fe6d126514d0c8735ddbfc7b8c2
ROG1 <934162496@qq.com>
2021-11-19 01:28:16 -0800

    l

--- project1/ex2/src/LCS.h
+++ project1/ex2/src/LCS.h
@@ -39,25 +39,25 @@ LCS::~LCS() {
 void LCS::GetLCS() {
     for (int i = 1;i <= len;i++) {
         for (int j = 1; j <= len; j++) {
-            if (x[i-1] == y[j-1])c[i][j] = c[i - 1][j - 1] + 1;
+            if (x[i - 1] == y[j - 1])c[i][j] = c[i - 1][j - 1] + 1;
             else c[i][j] = c[i - 1][j] >= c[i][j - 1] ? c[i - 1][j] : c[i][j - 1];
         }
     }
-    printf("%d",c[len][len]);
+    printf("%d", c[len][len]);
 }
 
-void LCS::PrintLCS(int i, int j, std::string str_lcs){
-    while (i>0 && j>0){
-        if(x[i-1]==y[j-1]){
-            str_lcs = x[i-1]+str_lcs;
+void LCS::PrintLCS(int i, int j, std::string str_lcs) {
+    while (i > 0 && j > 0) {
+        if (x[i - 1] == y[j - 1]) {
+            str_lcs = x[i - 1] + str_lcs;
             i--;
             j--;
         }
-        else if(c[i-1][j]<c[i][j-1]) j--;
-        else if(c[i-1][j]>c[i][j-1]) i--;
+        else if (c[i - 1][j] < c[i][j - 1]) j--;
+        else if (c[i - 1][j] > c[i][j - 1]) i--;
         else {
-            PrintLCS(i-1, j , str_lcs);
-            PrintLCS(i, j-1 ,str_lcs);
+            PrintLCS(i, j - 1, str_lcs);
+            PrintLCS(i - 1, j, str_lcs);
             return;
         }
     }
