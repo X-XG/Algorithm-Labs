3b1b3b7ef8243e7f2bd76f903b90394f8fd7e0ba
ROG1 <934162496@qq.com>
2021-12-03 18:58:10 -0800

    ex2

---
+++ project2/ex2/src/main.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <fstream>
+#include <chrono>
+
+#define NUM_MAX 30
+
+using namespace std;
+using namespace  chrono;
+
+typedef struct Node {
+    int rank;
+    Node* parent;
+}Node;
+
+void makeSet(Node* x) {
+    x->parent = x;
+    x->rank = 0;
+}
+
+void link(Node* x, Node* y) {
+    if (x->rank > y->rank) {
+        y->parent = x;
+    }
+    else {
+        x->parent = y;
+        if (x->rank == y->rank) {
+            y->rank++;
+        }
+    }
+}
+
+Node* findSet(Node* x) {
+    if (x != x->parent) {
+        x->parent = findSet(x->parent);
+    }
+    return x->parent;
+}
+
+void Union(Node* x, Node* y) {
+    link(findSet(x), findSet(y));
+}
+
+int main() {
+    string input_path = "../input/2_2_input.txt";
+    string result_path = "../output/result.txt";
+    string time_path = "../output/time.txt";
+
+    ifstream infile;
+    ofstream fresult;
+    ofstream ftime;
+    infile.open(input_path);
+    fresult.open(result_path);
+    ftime.open(time_path);
+
+    int M[NUM_MAX][NUM_MAX];
+    Node* set[NUM_MAX] = { nullptr };
+    for (int num = 10; num <= NUM_MAX; num += 5) {
+        for (int i = 0;i < num;i++) {
+            for (int j = 0;j < num;j++) {
+                infile >> M[i][j];
+            }
+            set[i] = new Node;
+        }
+
+        int count = 0;
+        auto start = system_clock::now();
+        for (int i = 0;i < num;i++) {
+            makeSet(set[i]);
+        }
+        for (int i = 0;i < num;i++) {
+            for (int j = i + 1;j < num;j++) {
+                if (M[i][j] == 1) {
+                    Union(set[i], set[j]);
+                }
+            }
+        }
+        for (int i = 0;i < num;i++) {
+            if (findSet(set[i]) == set[i]) {
+                count++;
+            }
+        }
+        auto end = system_clock::now();
+        auto duration = duration_cast<nanoseconds>(end - start);
+        int time = int(duration.count()) * nanoseconds::period::num;
+        ftime << time << endl;
+        fresult << "n=" << num << ' ' << count << endl;
+    }
+
+    return 0;
+}
